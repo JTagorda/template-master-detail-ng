e98d5ea18f9dac211beecdbe559af41d015aa672
boris <boris.karastanev@progress.com>
2017-07-05 13:55:56 +0300

    Extended preinstall script to delete folders recursively
    Signed-off-by: boris <boris.karastanev@progress.com>

--- tools/preinstall.js
+++ tools/preinstall.js
@@ -12,6 +12,18 @@ console.log("creating tslint.json to enable linting...");
 const tslintConfig = "tslint.json";
 copyConfig(tslintConfig);
 
+// Remove assets folder
+console.log('Removing assets folder');
+const assetsDir = path.join(__dirname, 'assets');
+try {
+    delFodler(assetsDir);
+} catch (err) {
+    console.error({
+        message: 'Error occured while removing assets folder',
+        error: err
+    });
+}
+
 function copyConfig(configFilename) {
     const oldPath = path.join(__dirname, configFilename);
     const newPath = path.join(getAppRootFolder(), configFilename);
@@ -20,6 +32,24 @@ function copyConfig(configFilename) {
             console.log(err);
         }
     });
+}
+
+function delFodler(folderPath) {
+    if (fs.statSync(folderPath).isDirectory()) {
+        fs.readdirSync(folderPath).forEach(function (file) {
+            let content = path.join(folderPath, file);
+
+            if (fs.statSync(content).isDirectory()) {
+                delFodler(content);
+            } else {
+                fs.unlinkSync(content);
+            }
+        });
+        fs.rmdirSync(folderPath);
+
+    } else {
+        return new Error('Path is not a Folder');
+    }
 }
 
 function getAppRootFolder() {
