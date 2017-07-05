c7c6d3e6279e7c946845490116235b3316fa1dbf
boris <boris.karastanev@progress.com>
2017-07-05 14:28:36 +0300

    Removed try catch block
    Signed-off-by: boris <boris.karastanev@progress.com>

--- tools/preinstall.js
+++ tools/preinstall.js
@@ -15,14 +15,7 @@ copyConfig(tslintConfig);
 // Remove assets folder
 console.log('Removing assets folder');
 const assetsDir = path.join(__dirname, 'assets');
-try {
-    delFodler(assetsDir);
-} catch (err) {
-    console.error({
-        message: 'Error occured while removing assets folder',
-        error: err
-    });
-}
+delFodler(assetsDir);
 
 function copyConfig(configFilename) {
     const oldPath = path.join(__dirname, configFilename);
@@ -46,9 +39,6 @@ function delFodler(folderPath) {
             }
         });
         fs.rmdirSync(folderPath);
-
-    } else {
-        return new Error('Path is not a Folder');
     }
 }
 
